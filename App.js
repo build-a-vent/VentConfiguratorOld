2e6c197b6e278e2ad76b41e352c8eb7a5259eaa7
Konstantin <konstantin.kroner@gmail.com>
2020-07-04 11:13:52 +0200

    add dependencies

--- App.js
+++ App.js
@@ -9,7 +9,7 @@
 import React from 'react';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-
+import { PermissionsAndroid } from 'react-native';
 
 import {
   Text,
@@ -26,7 +26,32 @@ import Footer from './Components/footer/Footer';
 
 
 const Stack = createStackNavigator();
-const App: () => React$Node = () => {
+
+async function getPermissions(params) {
+
+  const granted = await PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    {
+      title: 'Location permission is required for WiFi connections',
+      message:
+        'This app needs location permission as this is required  ' +
+        'to scan for wifi networks.',
+      buttonNegative: 'DENY',
+      buttonPositive: 'ALLOW',
+    },
+  );
+  if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+    // You can now use react-native-wifi-reborn
+    console.log('got access')
+  } else {
+    console.log('no access')
+    // Permission denied
+  }
+
+}
+
+const App = () => {
+  getPermissions();
   return (
     <>
       <Text>WORKS</Text>
